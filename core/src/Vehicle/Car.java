49af862f8bf5cfe9a1f74d5882a4112c72950765
Kevin Jetten <jetten.kevin@gmail.com>
2018-11-27 20:22:45 +0100

    Added buses to the simulation

--- core/src/Vehicle/Car.java
+++ core/src/Vehicle/Car.java
@@ -1,19 +1,13 @@
 package Vehicle;
 
-import Obstacles.Obstacle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-import java.util.List;
-
-import static com.ai.trafficsim.TrafficSimMain.screenHeight;
-import static com.ai.trafficsim.TrafficSimMain.screenWidth;
-
 public class Car extends Vehicle {
 
     public Car(int startPosition, Direction direction) {
-        super(2, 2, direction);
+        super(2, 2, direction, VehicleType.Car);
         set(new Sprite(new Texture("car.png")));
         SetStartPosition(startPosition);
         setX(locationX);
@@ -21,8 +15,9 @@ public class Car extends Vehicle {
         setColor(Color.BLUE);
     }
 
-    private void SetStartPosition(int startPosition) {
-        this.startposition = startPosition;
+    @Override
+    void SetStartPosition(int startPosition) {
+        this.startPosition = startPosition;
         switch (startPosition) {
             case 1:
                 locationX = 0;
@@ -81,106 +76,5 @@ public class Car extends Vehicle {
                 }
                 break;
         }
-    }
-
-    @Override
-    public boolean LocationUpdateTick(List<Obstacle> obstacleList, List<Vehicle> vehicleList) {
-        boolean isOverlapping = false;
-        previousX = locationX;
-        previousY = locationY;
-
-        if (turningPointY > 0 || turningPointX > 0) {
-            if ((turningPointY > (locationY - 5) && turningPointY < (locationY + 5)) || (turningPointX > (locationX - 5) && turningPointX < (locationX + 5))) {
-                MakeTurn();
-            }
-        }
-        if (horizontal) {
-            if (movingForward) {
-                locationX = locationX + maxSpeed;
-                setX(locationX);
-            } else {
-                locationX = locationX - maxSpeed;
-                setX(locationX);
-            }
-        } else {
-            if (movingForward) {
-                locationY = locationY + maxSpeed;
-                setY(locationY);
-            } else {
-                locationY = locationY - maxSpeed;
-                setY(locationY);
-            }
-        }
-
-        for (Obstacle o : obstacleList) {
-            if (getBoundingRectangle().overlaps(o.getBoundingRectangle())) {
-                if (!o.IsPassable()) {
-                    isOverlapping = true;
-                }
-            }
-        }
-
-        for (Vehicle v : vehicleList) {
-            if (v != this) {
-                if (getBoundingRectangle().overlaps(v.getBoundingRectangle())) {
-                    isOverlapping = true;
-                }
-            }
-        }
-
-        if (isOverlapping) {
-            locationX = previousX;
-            setX(locationX);
-            locationY = previousY;
-            setY(locationY);
-        }
-        return locationY > screenHeight || locationY < 0 || locationX > screenWidth || locationX < 0;
-    }
-
-    private void MakeTurn() {
-        switch (startposition) {
-            case 1:
-                if (direction == Direction.left) {
-                    horizontal = false;
-                    rotate(90);
-                } else if (direction == Direction.right) {
-                    horizontal = false;
-                    movingForward = false;
-                    rotate(270);
-                }
-                break;
-            case 2:
-                if (direction == Direction.left) {
-                    horizontal = true;
-                    movingForward = false;
-                    rotate(270);
-                } else if (direction == Direction.right) {
-                    horizontal = true;
-                    rotate(90);
-                }
-                break;
-            case 3:
-                if (direction == Direction.left) {
-                    horizontal = false;
-                    rotate(90);
-                } else if (direction == Direction.right) {
-                    horizontal = false;
-                    movingForward = true;
-                    rotate(270);
-                }
-                break;
-            case 4:
-                if (direction == Direction.left) {
-                    horizontal = true;
-                    movingForward = true;
-                    rotate(90);
-                } else if (direction == Direction.right) {
-                    horizontal = true;
-                    rotate(270);
-                }
-                break;
-        }
-        turningPointY = -1;
-        turningPointX = -1;
     }
 }
--- core/src/Vehicle/Vehicle.java
+++ core/src/Vehicle/Vehicle.java
@@ -5,38 +5,145 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import java.util.List;
 
+import static com.ai.trafficsim.TrafficSimMain.screenHeight;
+import static com.ai.trafficsim.TrafficSimMain.screenWidth;
+
 public abstract class Vehicle extends Sprite {
     int length;
-    int maxSpeed;
+    double maxSpeed;
     float locationX;
     float locationY;
     float previousX;
     float previousY;
     boolean horizontal;
     boolean movingForward;
-    int startposition;
+    int startPosition;
     Direction direction;
     float turningPointX;
     float turningPointY;
+    VehicleType vehicleType;
 
-    Vehicle(int length, int maxSpeed, Direction direction) {
+    Vehicle(int length, double maxSpeed, Direction direction, VehicleType vehicleType) {
         this.length = length;
         this.maxSpeed = maxSpeed;
         this.previousX = locationX;
         this.previousY = locationY;
         this.direction = direction;
+        this.vehicleType = vehicleType;
         turningPointX = -1;
         turningPointY = -1;
     }
 
+    abstract void SetStartPosition(int startPosition);
+
     public int GetStartingPosition() {
-        return startposition;
+        return startPosition;
     }
 
     public Direction GetDirection() {
         return direction;
     }
 
-    // Return false if the vehicle is of screen
-    abstract public boolean LocationUpdateTick(List<Obstacle> obstacleList, List<Vehicle> vehicleList);
+    public boolean LocationUpdateTick(List<Obstacle> obstacleList, List<Vehicle> vehicleList) {
+        boolean isOverlapping = false;
+        previousX = locationX;
+        previousY = locationY;
+
+        if (turningPointY > 0 || turningPointX > 0) {
+            if ((turningPointY > (locationY - 5) && turningPointY < (locationY + 5)) || (turningPointX > (locationX - 5) && turningPointX < (locationX + 5))) {
+                MakeTurn();
+            }
+        }
+        if (horizontal) {
+            if (movingForward) {
+                locationX = locationX + (float) maxSpeed;
+                setX(locationX);
+            } else {
+                locationX = locationX - (float) maxSpeed;
+                setX(locationX);
+            }
+        } else {
+            if (movingForward) {
+                locationY = locationY + (float) maxSpeed;
+                setY(locationY);
+            } else {
+                locationY = locationY - (float) maxSpeed;
+                setY(locationY);
+            }
+        }
+
+        for (Obstacle o : obstacleList) {
+            if (getBoundingRectangle().overlaps(o.getBoundingRectangle())) {
+                if (!o.IsPassable()) {
+                    isOverlapping = true;
+                }
+            }
+        }
+
+        for (Vehicle v : vehicleList) {
+            if (v != this) {
+                if (getBoundingRectangle().overlaps(v.getBoundingRectangle())) {
+                    isOverlapping = true;
+                }
+            }
+        }
+
+        if (isOverlapping) {
+            locationX = previousX;
+            setX(locationX);
+            locationY = previousY;
+            setY(locationY);
+        }
+        return locationY > screenHeight || locationY < 0 || locationX > screenWidth || locationX < 0;
+    }
+
+    public VehicleType GetVehicleType(){
+        return vehicleType;
+    }
+    private void MakeTurn() {
+        switch (startPosition) {
+            case 1:
+                if (direction == Direction.left) {
+                    horizontal = false;
+                    rotate(90);
+                } else if (direction == Direction.right) {
+                    horizontal = false;
+                    movingForward = false;
+                    rotate(270);
+                }
+                break;
+            case 2:
+                if (direction == Direction.left) {
+                    horizontal = true;
+                    movingForward = false;
+                    rotate(270);
+                } else if (direction == Direction.right) {
+                    horizontal = true;
+                    rotate(90);
+                }
+                break;
+            case 3:
+                if (direction == Direction.left) {
+                    horizontal = false;
+                    rotate(90);
+                } else if (direction == Direction.right) {
+                    horizontal = false;
+                    movingForward = true;
+                    rotate(270);
+                }
+                break;
+            case 4:
+                if (direction == Direction.left) {
+                    horizontal = true;
+                    movingForward = true;
+                    rotate(90);
+                } else if (direction == Direction.right) {
+                    horizontal = true;
+                    rotate(270);
+                }
+                break;
+        }
+        turningPointY = -1;
+        turningPointX = -1;
+    }
 }
